b29e26499586b19d46247618a81d1d5c3c756bfa
Ashok_Shiva <ashokkumarpalaki@gmail.com>
2021-06-23 12:57:43 +0530

    something

---
+++ Advanced_pract/60days/core_python.py
@@ -0,0 +1,10 @@
+class CorePython():
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    CorePython()
---
+++ Advanced_pract/60days/numpy_lib.py
@@ -0,0 +1,9 @@
+class NumpyLib():
+    def __init__(self):
+        pass
+    def main(self):
+        print("NumpyLib")
+
+if __name__ == '__main__':
+    num=NumpyLib()
+    num.main()
